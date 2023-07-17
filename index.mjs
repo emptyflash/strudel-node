38eb2ff921084d71a39ce5aaad4fbf463632d750
cameron <emptyflash@gmail.com>
2023-07-17 15:55:48 -0400

    Add package json and code

---
+++ index.mjs
@@ -0,0 +1,47 @@
+import { AudioContext } from 'node-web-audio-api';
+global.AudioContext = AudioContext
+
+import { controls, repl, evalScope } from '@strudel.cycles/core';
+import { samples, getAudioContext, webaudioOutput, initAudioOnFirstClick } from '@strudel.cycles/webaudio/dist/index.mjs';
+import { transpiler } from '@strudel.cycles/transpiler/dist/index.mjs';
+import readline from 'readline';
+
+const ctx = getAudioContext();
+
+const { evaluate } = repl({
+  defaultOutput: webaudioOutput,
+  getTime: () => ctx.currentTime,
+  transpiler,
+});
+
+await evalScope(
+  controls,
+  import('@strudel.cycles/mini/dist/index.mjs'),
+  import('@strudel.cycles/webaudio/dist/index.mjs'),
+);
+
+await samples('github:tidalcycles/Dirt-Samples/master')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on("close", function() {
+    process.exit(0);
+});
+
+function prompt() {
+    rl.question("strudel> ", (code) => {
+        if (code) {
+            try {
+                evaluate(code);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+        prompt();
+    });
+}
+
+prompt();
